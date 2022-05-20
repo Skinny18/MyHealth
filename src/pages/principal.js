37759e4ba39100dd6ca68fd748e07487100319d2
Skinny18 <gcgabriel257@gmail.com>
2022-05-20 13:39:58 -0400

    nooo

--- src/pages/principal.js
+++ src/pages/principal.js
@@ -11,7 +11,7 @@ function Princ() {
           <div id="geral">
 		<div id="cabecalho"> 
 			<img align="left" height="89" src={hlogo}/>
-			<Link to='/'><h1 id="h111" >My<span id="red">Health</span></h1></Link>
+			<h1 id="h111" >My<span id="red">Health</span></h1>
 		</div>
 		<div id="principal">
 			<p></p>
